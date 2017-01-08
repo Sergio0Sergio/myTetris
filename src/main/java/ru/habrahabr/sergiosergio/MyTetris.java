ecc0e1f32ceea8c3a8084a462806bf02b2f9b0c9
Sergei Grimanov <tosergi@gmail.com>
2017-01-08 23:34:27 +0300

    The MyTetris project has been started.

---
+++ src/main/java/ru/habrahabr/sergiosergio/MyTetris.java
@@ -0,0 +1,164 @@
+package ru.habrahabr.sergiosergio;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class MyTetris{
+
+    final String TITLE_OF_PROGRAMM = "Tetris";
+    final int BLOCK_SIZE = 25;
+    final int ARC_RADIUS = 6;
+    final int FIELD_WIDTH = 10;
+    final int FIELD_HEIGHT = 180;
+    final int START_LOCATION = 180;
+    final int FIELD_DX = 7;
+    final int FIELD_DY = 26;
+    final int LEFT = 37;
+    final int UP = 38;
+    final int RIGHT = 39;
+    final int DOWN = 40;
+    final int SHOW_DELAY = 350;
+    final int[][][] SHAPES = {
+            {{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {4, 0x00f0f0}}, // I
+            {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {4, 0xf0f000}}, // O
+            {{1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x0000f0}}, // J
+            {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xf0a000}}, // L
+            {{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x00f000}}, // S
+            {{1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xa000f0}}, // T
+            {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xf00000}}  // Z
+    };
+    final int[] SCORES = {100, 300, 700, 1500};
+    int gameScore = 0;
+    int [][] mine = new int[FIELD_HEIGHT+1][FIELD_WIDTH];
+    JFrame frame;
+    Canvas canvas = new Canvas();
+    Random random = new Random();
+    Figure figure = new Figure();
+    boolean gameOver = false;
+    final int[][] GAME_OVER_MSG = {
+            {0,1,1,0,0,0,1,1,0,0,0,1,0,1,0,0,0,1,1,0},
+            {1,0,0,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,1},
+            {1,0,1,1,0,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1},
+            {1,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,0},
+            {0,1,1,0,0,1,0,0,1,0,1,0,1,0,1,0,0,1,1,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,1,1,0,0,1,0,0,1,0,0,1,1,0,0,1,1,1,0,0},
+            {1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0},
+            {1,0,0,1,0,1,0,1,0,0,1,1,1,1,0,1,1,1,0,0},
+            {1,0,0,1,0,1,1,0,0,0,1,0,0,0,0,1,0,0,1,0},
+            {0,1,1,0,0,1,0,0,0,0,0,1,1,0,0,1,0,0,1,0}
+    };
+
+    public static void main( String[] args ){
+
+        new MyTetris().go();
+    }
+
+    void checkFilling(){
+
+    }
+
+    void go(){
+
+        frame = new JFrame(TITLE_OF_PROGRAMM);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
+        frame.setLocation(START_LOCATION, START_LOCATION);
+        frame.setResizable(false);
+        canvas.setBackground(Color.black);
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (!gameOver){
+                    if (e.getKeyCode() == DOWN) figure.drop();
+                    if (e.getKeyCode() == UP) figure.rotate();
+                    if (e.getKeyCode() == LEFT || e.getKeyCode() == RIGHT) figure.move(e.getKeyCode());
+                }
+                canvas.repaint();
+            }
+        });
+        frame.getContentPane().add(BorderLayout.CENTER, canvas);
+        frame.setVisible(true);
+
+        Arrays.fill(mine[FIELD_HEIGHT], 1);
+
+        while (!gameOver){
+
+            try {
+                Thread.sleep(SHOW_DELAY);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            canvas.repaint();
+            if (figure.isTouchTheGround()){
+                figure.leaveOnTheGround();
+                checkFilling();
+                figure = new Figure();
+                gameOver = figure.isCrossGround();
+            }else{
+
+                figure.stepDown();
+            }
+        }
+
+    }
+
+    class Figure{
+
+        boolean isTouchTheGround(){
+
+            return false;
+
+        }
+
+        boolean isCrossGround(){
+
+            return false;
+
+        }
+
+        void leaveOnTheGround(){
+
+        }
+
+        void stepDown(){
+
+        }
+
+        void move(int direction){
+
+        }
+
+        void drop(){
+
+        }
+
+        void rotate(){
+
+        }
+
+
+
+    }
+
+    class Block{
+
+    }
+
+    class Canvas extends JPanel{
+
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+        }
+    }
+}
